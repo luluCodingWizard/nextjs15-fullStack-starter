5b9d61e6a5f2cb47daf91cbc2e7dabb15a4cc85a
luluCodingWizard <how.to.develop.fast@gmail.com>
2025-01-28 13:32:22 +1100

    feat: chapter 24 fetch products in the client

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,20 @@
 import Products from "../components/Products";
-import { productsData } from "../data";
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  let products = [];
+  try {
+    const response = await fetch("http://localhost:3000/api/products");
+    if (!response.ok) {
+      throw new Error("failed to fetch products!");
+    }
+    products = await response.json();
+  } catch (error) {
+    console.error(error);
+    return <div> Error Loading the Products please try again</div>;
+  }
   return (
     <>
       <h1>Products</h1>
-      <Products products={productsData} />
+      <Products products={products} />
     </>
   );
 }
